ed5201ee6ced525d1df4320843da94f2b4476c75
hudaxian <whym21@outlook.com>
2019-01-06 16:40:14 +0800

    点点连线

--- canvas画板/canvas画圆/main.js
+++ canvas画板/canvas画圆/main.js
@@ -5,23 +5,40 @@ function drawCircle(x,y,radius){
 //圆形
 context.beginPath();
 context.arc(x,y,radius,0,Math.PI*2);
-context.stroke();
+context.fill();
 }
 
-var painting = false
-yyy.onmousedown = function(aaa){
-  painting = true
-  var x = aaa.clientX
-  var y = aaa.clientY
-  drawCircle(x,y,5)
+function drawline(x1,y1,x2,y2){
+  context.beginPath();
+
+  context.lineWidth = 4;
+  context.moveTo(x1,y1);
+  context.lineTo(x2,y2);
+  context.stroke();
+  context.closePath();
 }
+
+var painting = false;
+var lastPoint = {x:undefined,y:undefined};
+yyy.onmousedown = function(aaa){
+  painting = true;
+  var x = aaa.clientX;
+  var y = aaa.clientY;
+  lastPoint = {x:x,y:y};
+  drawCircle(x,y,2);
+}
+
 yyy.onmousemove = function(aaa){
   if(painting){
-      var x = aaa.clientX
-      var y = aaa.clientY
-      drawCircle(x,y,5)
-  }else{}
+      var x = aaa.clientX;
+      var y = aaa.clientY;
+      var newPoint = {x:x,y:y};
+      drawCircle(x,y,2);
+      drawline(lastPoint.x,lastPoint.y,newPoint.x,newPoint.y);
+      lastPoint = newPoint;
+  }
 }
 yyy.onmouseup = function(aaa){
-  painting = false
+  painting = false;
+  
 }
