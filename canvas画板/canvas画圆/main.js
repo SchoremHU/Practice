26f1ca5d853d43b75fb68a24f4af9a6a0c9b4166
hudaxian <whym21@outlook.com>
2019-01-06 16:49:53 +0800

    点点相连

--- canvas画板/canvas画圆/main.js
+++ canvas画板/canvas画圆/main.js
@@ -1,23 +1,6 @@
 var yyy = document.getElementById('xxx');
 var context = yyy.getContext('2d');
 
-function drawCircle(x,y,radius){
-//圆形
-context.beginPath();
-context.arc(x,y,radius,0,Math.PI*2);
-context.fill();
-}
-
-function drawline(x1,y1,x2,y2){
-  context.beginPath();
-
-  context.lineWidth = 4;
-  context.moveTo(x1,y1);
-  context.lineTo(x2,y2);
-  context.stroke();
-  context.closePath();
-}
-
 var painting = false;
 var lastPoint = {x:undefined,y:undefined};
 yyy.onmousedown = function(aaa){
@@ -40,5 +23,22 @@ yyy.onmousemove = function(aaa){
 }
 yyy.onmouseup = function(aaa){
   painting = false;
-  
 }
+
+function drawCircle(x,y,radius){
+  //圆形
+  context.beginPath();
+  context.arc(x,y,radius,0,Math.PI*2);
+  context.fill();
+  }
+  
+  function drawline(x1,y1,x2,y2){
+    context.beginPath();
+  
+    context.lineWidth = 4;
+    context.moveTo(x1,y1);
+    context.lineTo(x2,y2);
+    context.stroke();
+    context.closePath();
+  }
+  
