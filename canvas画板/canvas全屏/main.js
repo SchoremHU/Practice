80e2e3940e6083dc1c40ca26b54e0c4d4c44c14b
hudaxian <whym21@outlook.com>
2019-01-07 10:58:30 +0800

    添加橡皮擦及封装函数

--- canvas画板/canvas全屏/main.js
+++ canvas画板/canvas全屏/main.js
@@ -1,48 +1,88 @@
 var yyy = document.getElementById('xxx');
 var context = yyy.getContext('2d');
 
- ccc()
-window.onresize = function(){
-   ccc()
+var usingEraser = false;
+eraser.onclick = function () {
+  usingEraser = true;
+  eraser.textContent = '正在使用橡皮擦';
+  brush.textContent = '画笔';
+}
+brush.onclick = function () {
+  usingEraser = false;
+  brush.textContent = '正在使用画笔';
+  eraser.textContent = '橡皮擦';
 }
 
-var painting = false;
-var lastPoint = {x:undefined,y:undefined};
-yyy.onmousedown = function(aaa){
-  painting = true;
-  var x = aaa.clientX;
-  var y = aaa.clientY;
-  lastPoint = {x:x,y:y};
-}
 
-yyy.onmousemove = function(aaa){
-  if(painting){
-      var x = aaa.clientX;
-      var y = aaa.clientY;
-      var newPoint = {x:x,y:y};
-      drawline(lastPoint.x,lastPoint.y,newPoint.x,newPoint.y);
-      lastPoint = newPoint;
+/******总体******/
+autocanvasSize(yyy);
+
+ListenToMouse(yyy);
+
+
+
+
+/*******封装函数*******/
+function autocanvasSize(canvasSize) {
+  setCanvasSize();
+  window.onresize = function () {
+    setCanvasSize();
+  }
+  function setCanvasSize() {
+    var pageWidth = document.documentElement.clientWidth;
+    var pageHeight = document.documentElement.clientHeight;
+
+    canvasSize.width = pageWidth;
+    canvasSize.height = pageHeight;
   }
 }
-yyy.onmouseup = function(aaa){
-  painting = false;
-  
-}
 
-function ccc(){
-  var pageWidth = document.documentElement.clientWidth
-  var pageHeight = document.documentElement.clientHeight
-  
-  yyy.width = pageWidth
-  yyy.height = pageHeight;
-}
 
-function drawline(x1,y1,x2,y2){
+function drawline(x1, y1, x2, y2) {
   context.beginPath();
 
   context.lineWidth = 4;
-  context.moveTo(x1,y1);
-  context.lineTo(x2,y2);
+  context.moveTo(x1, y1);
+  context.lineTo(x2, y2);
   context.stroke();
   context.closePath();
 }
+
+function ListenToMouse(canvas) {
+  var painting = false;
+  var lastPoint = { x: undefined, y: undefined };
+  canvas.onmousedown = function (aaa) {
+    var x = aaa.clientX;
+    var y = aaa.clientY;
+    if (usingEraser) {
+      painting = true
+      context.clearRect(x - 5, y - 5, 10, 10)
+    } else {
+      painting = true;
+      lastPoint = { x: x, y: y };
+    }
+  }
+
+  canvas.onmousemove = function (aaa) {
+    var x = aaa.clientX;
+    var y = aaa.clientY;
+    if (!painting) { return }
+    if (usingEraser) {
+
+      context.clearRect(x - 5, y - 5, 10, 10)
+
+    } else {
+
+      var newPoint = { x: x, y: y };
+      drawline(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y);
+      lastPoint = newPoint;
+
+    }
+  }
+  canvas.onmouseup = function (aaa) {
+    painting = false;
+
+  }
+
+}
+
